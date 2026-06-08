bb967da0baa48fbd7de3b39259a23e760698e789
ZorialCart <zorialcart@outlook.com>
2026-06-07 18:07:01 -0700

    tercer commit: se agrega script

---
+++ CurriculumVitae/scrip.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.querySelector(".cv-container");
+  setTimeout(() => {
+    contenedor.classList.add("visible");
+  }, 100);
+});
